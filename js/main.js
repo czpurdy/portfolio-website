00d4ea72cabbdaf45b398509b91f56d8490d8d64
czpurdy <czpurdy@gmail.com>
2018-04-23 17:33:59 -0400

    no fallback to pdfjs

--- js/main.js
+++ js/main.js
@@ -193,8 +193,7 @@ $(document).ready(function(){
   // Resume options
   let options = {
     pdfOpenParams: { pagemode: 'none', scrollbar: '1', toolbar: '1'},
-    fallbackLink: '<p>This browser does not support inline PDFs. Please download the PDF to view it: <a href="./documents/CZPurdy_Resume.pdf">Download PDF</a></p>',
-    PDFJS_URL: "./pdfjs/web/viewer.html"
+    fallbackLink: '<p>This browser does not support inline PDFs. Please download the PDF to view it: <a href="./documents/CZPurdy_Resume.pdf">Download PDF</a></p>'
   };
   // Embed resume
   PDFObject.embed("./documents/CZPurdy_Resume.pdf", "#resume", options);
